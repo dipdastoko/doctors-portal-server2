1f0dbf6278d9fafc7283f13cc59a20d548265c10
dipdastoko <dipdastoko@gmail.com>
2022-08-26 01:54:20 +0600

    finished

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express')
 const app = express();
+const admin = require("firebase-admin");
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqquk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -19,19 +42,25 @@ async function run() {
         const appointmentsCollection = database.collection('appointments');
         const userCollection = database.collection('users');
 
-        app.get('/appointments', async (req, res) => {
-            const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
-            const query = { email: email, date: date };
-            const cursor = appointmentsCollection.find(query);
-            const appointments = await cursor.toArray();
-            res.json(appointments);
+        app.get('/appointments', verifyToken, async (req, res) => {
+            const requester = req.decodedEmail;
+            if (requester) {
+                const email = req.query.email;
+                const date = new Date(req.query.date).toLocaleDateString();
+                const query = { email: email, date: date };
+                const cursor = appointmentsCollection.find(query);
+                const appointments = await cursor.toArray();
+                res.json(appointments);
+            }
+            else {
+                res.status(403).json({ message: "not verified user" })
+            }
+
         });
 
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
-            console.log(result);
             res.json(result);
         });
 
@@ -49,7 +78,6 @@ async function run() {
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
-            console.log(result);
             res.json(result);
         });
 
@@ -62,12 +90,23 @@ async function run() {
             res.json(result);
         });
 
-        app.put('/users/admin', async (req, res) => {
+        app.put('/users/admin', verifyToken, async (req, res) => {
             const user = req.body;
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: 'admin' } };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.json(result);
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await userCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                req.status(403).json({ message: 'you do not have access' });
+            }
+
+
         })
 
     }
